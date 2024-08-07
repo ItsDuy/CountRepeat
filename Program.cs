2e12c00e7ac9f573df9a6324a2b9128526415fe0
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-07 14:34:05 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountRepeat;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Hello World! and Have A Nice Day";
+        char character;
+        
+        Console.Write("Enter a character: ");
+        character = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == character)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"The character '{character}' appears {count} times in the string.");
+    }
+}
